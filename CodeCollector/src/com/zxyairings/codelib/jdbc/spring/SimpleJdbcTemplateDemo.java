012b99dcff50dd318a293d2b6ad39edbec30b502
airings <zz_zxy@163.com>
2015-07-05 23:24:45 +0800

    add notes for spring jdbc simplejdbctemplate

--- CodeCollector/src/com/zxyairings/codelib/jdbc/spring/SimpleJdbcTemplateDemo.java
+++ CodeCollector/src/com/zxyairings/codelib/jdbc/spring/SimpleJdbcTemplateDemo.java
@@ -7,6 +7,11 @@ import com.zxyairings.codelib.jdbc.domain.User;
 import com.zxyairings.codelib.jdbc.util.JdbcUtilsDBCP;
 
 //这种使用方法所返回的结果就是正确的对象，它是使用了泛型。不用在将Object转型了
+/*
+ * SimpleJdbcTemplate内部包含了一个NamedParameterJdbcTemplate;
+ * 所以NamedParameterJdbcTemplate能做的事情SimpleJdbcTemplate都能干，
+ * SimpleJdbcTemplate相对于NamedParameterJdbcTemplate主要增加了JDK5.0的泛型和可变长度参数支持。
+ */
 
 public class SimpleJdbcTemplateDemo {
 	static SimpleJdbcTemplate simple = new SimpleJdbcTemplate(JdbcUtilsDBCP
